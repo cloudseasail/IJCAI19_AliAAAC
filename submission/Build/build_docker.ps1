29a4cb40c57a434a8c2ccbba2a36bfb3538e0942
William Yin (Shuyang) <ywilliam@qti.qualcomm.com>
2019-03-05 22:45:51 +0800

    defense submission

--- submission/Build/build_docker.ps1
+++ submission/Build/build_docker.ps1
@@ -1,4 +1,4 @@
-$tag="d2.2.2"
+$tag="d2.3.1"
 #$folder="..\NonTargetAttack"
 # $folder="..\TargetAttack"
 $folder="..\Defense"
@@ -7,8 +7,10 @@ cp ..\..\IJCAI19 .\IJCAI19 -Force -Recurse
 cp $folder\run.sh .\run.sh -Force 
 cp $folder\attack.py .\attack.py -Force 
 
-docker build -f Dockerfile -t registry.cn-hangzhou.aliyuncs.com/haifan_deep/ijcai2019_pilot:$tag .\
+docker build -f Dockerfile -t registry.cn-shanghai.aliyuncs.com/haifan_deep/ijcai2019:$tag .\
 
 rm .\IJCAI19 -Recurse
 rm .\run.sh -Force 
 rm .\attack.py -Force 
+
+docker push registry.cn-shanghai.aliyuncs.com/haifan_deep/ijcai2019:$tag
